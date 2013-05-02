084a8b3649b9587d24b1cae6dc360ee31179c0d4
Michal <michal@Michals-Mac.local>
2013-05-02 14:24:40 +0200

    Initial Commit

---
+++ SampleAppTests/SampleAppTests.h
@@ -0,0 +1,13 @@
+//
+//  SampleAppTests.h
+//  SampleAppTests
+//
+//  Created by Michal on 5/2/13.
+//  Copyright (c) 2013 Michal. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SampleAppTests : SenTestCase
+
+@end
---
+++ SampleAppTests/SampleAppTests.m
@@ -0,0 +1,32 @@
+//
+//  SampleAppTests.m
+//  SampleAppTests
+//
+//  Created by Michal on 5/2/13.
+//  Copyright (c) 2013 Michal. All rights reserved.
+//
+
+#import "SampleAppTests.h"
+
+@implementation SampleAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SampleAppTests");
+}
+
+@end
